47ef12c41e5d9544326693160d7a8fdc0e2cbe0f
nolteman0815 <nolteman@gmx.de>
2017-03-10 00:42:12 +0100

    basic xml layout part 1

---
+++ app/src/main/java/com/example/android/ltype/LType.java
@@ -0,0 +1,13 @@
+package com.example.android.ltype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ltype);
+    }
+}
